9a971526f62ced40498f5b21e31ab612b6d43e16
“Atagowick” <Atagoempire@gmail.com>
2024-07-04 14:40:29 -0500

    Creacion del body del index

---
+++ Inicio/body.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fusion</title>
+    <link rel="stylesheet" href="../Estilos/Body.css">
+</head>
+
+<body class="fondo-inicio">
+    <div class="oscuro">
+    </div>
+    
+    <H2></H2>
+    <div class="">
+        <p class="titulo">Sobre Nosotros</p>
+        
+        <p class="parrafo">Situado en la ciudad de ILO, RESTAURANT FUSION. Se distingue por ofrecer una fusión única de sabores locales e internacionales.
+            Nuestro compromiso con ingredientes frescos y técnicas culinarias innovadoras garantiza una experiencia gastronómica excepcional.
+        </p>
+    </div>
+    
+    <button>Ver mas</button>
+    <img class="" src="https://media.discordapp.net/attachments/1226725804961042544/1258451971094872095/circuito_ilo_playas.png?ex=6688183a&is=6686c6ba&hm=3091b28c54d1fc60dc14deec275a122230b5a4d62841ccfef2b83186737a0c4a&=&format=webp&quality=lossless&width=550&height=309" alt="">
+    <H2>Platos recomendados</H2>
+
+</body>
