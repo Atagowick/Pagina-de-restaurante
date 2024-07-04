34ec1073812c3a77bcc6dbfb189ac9b98d36a02b
Sebastian-ds <lupacaccosisebastian@gmail.com>
2024-07-04 14:57:48 -0500

    FOOTER CAMBIANDO

---
+++ Inicio/footer.php
@@ -0,0 +1,130 @@
+<footer>
+<!-- DISEÑO A CAMBIAR XDXDXDXDXDXDXDXDXDXDXDXD -->
+    <style>
+        footer {
+    background-color: #333;
+    color: #fff;
+    padding: 20px 0;
+  }
+  
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 0 20px;
+  }
+  
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 20px;
+  }
+  
+  .col-md-4 {
+    flex: 1 0 33.33%;
+    padding: 0 10px;
+  }
+  
+  h3 {
+    margin-bottom: 10px;
+  }
+  
+  p {
+    line-height: 1.5;
+  }
+  
+  .social-media {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  
+  .social-media li {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  
+  .social-media a {
+    display: inline-block;
+    width: 32px;
+    height: 32px;
+    text-align: center;
+    line-height: 32px;
+    border-radius: 50%;
+    background-color: #ddd;
+    color: #000;
+    transition: background-color 0.3s ease;
+  }
+  
+  .social-media a:hover {
+    background-color: #000;
+    color: #fff;
+  }
+
+  
+    </style>
+
+<!-- FIN DE DISEÑO XDXDXDXDXDXDXDXDXDXDXD -->
+
+<!--
+<footer style="background-color: #333; color: white; padding: 20px;">
+    <div style="display: flex; justify-content: space-between;">
+        <div>
+            <h3>Ubicación</h3>
+            
+
+            
+        </div>
+        <div>
+            <h3>Contacto</h3>
+            <p style="color: blue;">99563214</p>
+            <p>Fusion@gmail.com</p>
+            <p><i class="far fa-clock"></i> 09:00 AM - 17:00 PM</p>
+        </div>
+        <div>
+            <h3>Enlace rápido</h3>
+            <ul>
+                <li><u>Menu</u></li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </div>
+</footer>
+-->
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h3>Ubicación</h3>
+        <p>
+          99563214<br>
+          <a href="#">Enlace rápido</a>
+        </p>
+      </div>
+
+      <div class="col-md-4">
+        <h3>Contacto</h3>
+        <p>
+          <a href="mailto:Fusion@gmail.com">Fusion@gmail.com</a><br>
+          09:00 AM - 05:00 PM
+        </p>
+      </div>
+
+      <div class="col-md-4">
+        <h3>Redes sociales</h3>
+        <ul class="social-media">
+          <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <p>&copy; 2024 Fusion. Todos los derechos reservados.</p>
+      </div>
+    </div>
+  </div>
+</footer>
