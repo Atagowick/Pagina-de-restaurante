1e30e0c1f003e50ba055b425016ee1184d594fe2
atagowick <atagoempire@gmial.com>
2024-07-04 21:04:47 -0500

    nuevos cambios

--- Inicio/body.php
+++ Inicio/body.php
@@ -23,7 +23,6 @@
 
     </div>
     <button>Ver mas</button>
-    <img class="" src="https://media.discordapp.net/attachments/1226725804961042544/1258451971094872095/circuito_ilo_playas.png?ex=6688183a&is=6686c6ba&hm=3091b28c54d1fc60dc14deec275a122230b5a4d62841ccfef2b83186737a0c4a&=&format=webp&quality=lossless&width=550&height=309" alt="">
-    <H2>Platos recomendados</H2>
+    <p class="titulo-2">Platos recomendados</p>
 
 </body>
