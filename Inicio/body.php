415f05a08a64285608830b773f45744144173b96
“Atagowick” <Atagoempire@gmail.com>
2024-07-10 15:26:31 -0500

    Nuevos cambios

--- Inicio/body.php
+++ Inicio/body.php
@@ -5,24 +5,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fusion</title>
     <link rel="stylesheet" href="../Estilos/Body.css">
+    <!-- Enlazar Bootstrap CSS desde un CDN -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 
 <body class="fondo-inicio">
     <div class="oscuro">
     </div>
     
-    <H2></H2>
-    <div class="parrafo">
-        <p class="titulo">Sobre Nosotros</p>
-        
-        <p class="parrafo">Situado en la ciudad de ILO, RESTAURANT FUSION. Se distingue por ofrecer una fusión única de sabores locales e internacionales.
-            Nuestro compromiso con ingredientes frescos y técnicas culinarias innovadoras garantiza una experiencia gastronómica excepcional.
-        </p>
+    <div class="container mt-5">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h2>Sobre Nosotros</h2>
+                <p>Situado en la ciudad de ILO, RESTAURANT FUSION se distingue por ofrecer una fusión única de sabores locales e internacionales.
+                   Nuestro compromiso con ingredientes frescos y técnicas culinarias innovadoras garantiza una experiencia gastronómica excepcional.
+                </p>
+            </div>
+            <div class="col-md-6">
+                <img src="../img/imagen-sobre-nosotros.jpg" alt="Sobre Nosotros" class="img-fluid">
+            </div>
+        </div>
     </div>
     <div class="oscuro-2">
 
     </div>
     <button>Ver mas</button>
     <p class="titulo-2">Platos recomendados</p>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="image-overlay-container">
+                    <img src="../img/imagen-1.jpg" alt="Imagen 1" class="img-fluid">
+                    <div class="image-overlay">
+                        <div class="text">Descripción de la Imagen 1</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="image-overlay-container">
+                    <img src="../img/imagen-2.jpg" alt="Imagen 2" class="img-fluid">
+                    <div class="image-overlay">
+                        <div class="text">Descripción de la Imagen 2</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="image-overlay-container">
+                    <img src="../img/imagen-3.jpg" alt="Imagen 3" class="img-fluid">
+                    <div class="image-overlay">
+                        <div class="text">Descripción de la Imagen 3</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
