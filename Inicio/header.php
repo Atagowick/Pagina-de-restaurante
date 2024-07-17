58a71655436038225f5d6db6722791e975ce6e4a
atagowick <atagoempire@gmail.com>
2024-07-16 20:34:45 -0500

    Cambios de Header

---
+++ Inicio/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../Estilos/header.css">
+</head>
+    <header class="header">
+            <div class="row align-items-left">
+                <div class="col-md-1">
+                    <img src="../img/logo.png" alt="Fusion Logo" class="img-fluid logo">
+                </div>
+                <div class="col-md-3 text-left">
+                    <h1 class="titulo-header">Fusion</h1>
+                </div>
+                <div class="col-md-6 text-right">
+                    <nav class="nav">
+                        <a class="nav-link" href="#">Reservaciones</a>
+                        <a class="nav-link" href="#">Ofertas</a>
+                        <a class="nav-link" href="#">Opiniones</a>
+                    </nav>
+                </div>
+            </div>
+    </header>
