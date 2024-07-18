c6b8edc74a92685ac6be285527d518a4d1046120
Sebastian-ds <lupacaccosisebastian@gmail.com>
2024-07-18 04:05:20 -0500

    login

---
+++ admin/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesión</title>
+</head>
+<body>
+    <h2>Inicio de Sesión</h2>
+    <form action="login_next.php" method="post">
+        <label for="dni">DNI:</label><br>
+        <input type="text" id="dni" name="dni" required><br>
+        <label for="password">Contraseña:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Iniciar Sesión">
+    </form>
+</body>
+</html>
---
+++ admin/login_next.php
@@ -0,0 +1,35 @@
+<?php
+include "../conexion/conexion.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $dni = $_POST['dni'];
+    $password = $_POST['password'];
+
+    // Preparar y enlazar
+    $stmt = $conexion->prepare("SELECT * FROM Admin WHERE Dni = ? AND Contrasena = ?");
+    $stmt->bind_param("is", $dni, $password);
+
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // Inicio de sesión exitoso, mostrar alerta y redirigir después de unos segundos
+        echo "<script>
+            alert('Inicio de sesión exitoso');
+            setTimeout(function() {
+                window.location.href = '../Inicio/body.php';
+            }, 1000);                      // 1 segundos de espera
+        </script>";
+    } else {
+        echo "<script>
+        alert('DNI o Contraseña incorrectas');
+        setTimeout(function() {
+            window.location.href = '../admin/login.php';
+        }, 0);                      // 0 segundos de espera
+    </script>";
+    }
+
+    $stmt->close();
+}
+$conexion->close();
+?>
